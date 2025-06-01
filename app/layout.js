cfda2777202dea55e8af17952cf192160d3d7c76
Kshitij Singh <142705421+KshitijSingh07@users.noreply.github.com>
2025-06-01 20:02:23 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -38,7 +38,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+    layout: {
+      unsafe_disableDevelopmentModeWarnings: true,
+    },
+  }}>
       <html lang="en" className="dark">
         <head>
           <link
